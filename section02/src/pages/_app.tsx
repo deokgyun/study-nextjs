75f53dfd286bf2638c5ae67494384943006276d9
deokgyun <micron6594@naver.com>
2024-12-11 22:55:20 +0900

    prefetch: Link는 자동으로 가져오고 router.prefetch로 다른 페이지도 사전에 데이터를 불러올 수 있다.

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,10 +2,13 @@ import "@/styles/globals.css";
 import type {AppProps} from "next/app";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 // 루트 컴포넌트이다. 모든 페이지에 공통으로 적용되는 컴포넌트이다.
 // Component는 페이지 컴포넌트이다.
 // pageProps는 페이지 컴포넌트에 전달되는 props이다. props는 페이지 컴포넌트의 getServerSideProps, getStaticProps 함수에서 반환된다.
+
+// prefetch는 사전
 export default function App({Component, pageProps}: AppProps) {
 
   const router = useRouter();
@@ -14,13 +17,16 @@ export default function App({Component, pageProps}: AppProps) {
     router.push("/test");
   }
 
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
 
   return (
       <>
         <header>
           <Link href={"/"}>index</Link>
           &nbsp;
-          <Link href={"/search"}>search</Link>
+          <Link href={"/search"} prefetch={false}>search</Link>
           &nbsp;
           <Link href={"/book/1"}>book/1</Link>
           <div>
