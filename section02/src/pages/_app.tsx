79b39d59f6c9526a15d3539c041a31910378e3fa
deokgyun <micron6594@naver.com>
2024-12-12 22:55:49 +0900

    layout-header, footer, main 등 css등 설정

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
-import Link from "next/link";
-import {useRouter} from "next/router";
-import {useEffect} from "react";
+import GlobalLayout from "@/components/global-layout";
 
 // 루트 컴포넌트이다. 모든 페이지에 공통으로 적용되는 컴포넌트이다.
 // Component는 페이지 컴포넌트이다.
@@ -11,29 +9,9 @@ import {useEffect} from "react";
 // prefetch는 사전
 export default function App({Component, pageProps}: AppProps) {
 
-  const router = useRouter();
-
-  const onClickButton = () => {
-    router.push("/test");
-  }
-
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-      <>
-        <header>
-          <Link href={"/"}>index</Link>
-          &nbsp;
-          <Link href={"/search"} prefetch={false}>search</Link>
-          &nbsp;
-          <Link href={"/book/1"}>book/1</Link>
-          <div>
-            <button onClick={onClickButton}>/test 페이지로 이동</button>
-          </div>
-        </header>
+      <GlobalLayout>
         <Component {...pageProps} />
-      </>
+      </GlobalLayout>
   )
 }
